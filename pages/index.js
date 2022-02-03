7ece41605131e6b5dd9bbd800523761c55ac91c0
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2022-02-03 13:51:56 +0330

    link [eventId] page to firebase

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import Parse from "parse";
 
 function HomePage(props) {
   // const featuredEvents = getFeaturedEvents();
+  // () => {
+  //   Parse.initialize(
+  //     "lGQ805rTTSVHXzS74loDrd22GN8mOvWAr8siI8pb",
+  //     "8ePR4FUKlaAyw6cZMuFsIRLUWpj0z8zoigl3elKw"
+  //   );
+  //   Parse.serverURL = "https://parseapi.back4app.com/";
+  //   const myEvents = new Parse.Query("nextjs_demo");
+  //   console.log(myEvents);
+  // };
+
 
   return (
     <div>
